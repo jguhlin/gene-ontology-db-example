7c18a00b4c2ef49034dc2edb6aaf86ae3f946417
Joseph Guhlin <joseph.guhlin@gmail.com>
2013-05-27 05:52:45 -0500

    Initial commit

---
+++ bin/gene_ontology_db_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns gene-ontology-db-example.core-test
+  (:use clojure.test
+        gene-ontology-db-example.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gene-ontology-db-example
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
